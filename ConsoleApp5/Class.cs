105fc3641b86d014df5600d398c302e70d7e7e87
User <User@DESKTOP-3V2P9PB>
2024-04-07 11:18:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Class.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+	public abstract class AbstractPerson
+	{
+		public abstract string Name { get; set; }
+		public abstract int Number { get; set; }
+		public abstract void Say(string wordl);
+	}
+
+	public class X : AbstractPerson
+	{
+		public override string Name { get; set; }
+		public override int Number { get; set; }
+		public override void Say(string wordl)
+		{
+			Console.WriteLine(wordl);
+		}
+
+	}
+	public class Nick : X
+	{
+        public override string Name { get => base.Name += " Boss"; 
+			set  {
+				if (value == " ")
+				{
+					value = "Bob";
+				}
+				base.Name = value;
+			}
+		}
+        public override int Number { get => base.Number% 8;
+			set
+			{
+				if (value == null)
+				{
+					Random random = new Random();
+					random.Next();
+				}
+				base.Number = value;
+			} 
+		}
+        public override void Say(string wordl)
+        {
+            base.Say(wordl); Console.WriteLine($"My name {Name} and my number {Number}");
+        }
+
+    }
+	public class Start
+	{
+		public Start() { 
+			Nick nick = new Nick() {Name = " ", Number = 9 };
+			nick.Say("Hi");
+		}
+	}
+
+}
